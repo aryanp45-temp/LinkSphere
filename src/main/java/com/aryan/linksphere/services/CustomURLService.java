046976d4fa258f04cdecb26f1f873b23e552b6d6
Aryan Patil <aryanitinpatil@gmail.com>
2024-08-17 23:37:30 +0530

    Create CustomURLService.java

---
+++ src/main/java/com/aryan/linksphere/services/CustomURLService.java
@@ -0,0 +1,10 @@
+package com.aryan.linksphere.services;
+
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+// TODO: handle the generation and validation of custom URLs.
+@Service
+@Transactional
+public class CustomURLService {
+}
