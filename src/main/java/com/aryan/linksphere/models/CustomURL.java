eb44146ea3b06f3fa8785236f5ea24f7c101dffc
Aryan Patil <aryanitinpatil@gmail.com>
2024-08-17 23:37:16 +0530

    Create CustomURL.java

---
+++ src/main/java/com/aryan/linksphere/models/CustomURL.java
@@ -0,0 +1,45 @@
+package com.aryan.linksphere.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+
+@Entity
+@Table(name = "custom_urls")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CustomURL {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "custom_url", nullable = false, unique = true, length = 50)
+    private String customUrl;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+}
